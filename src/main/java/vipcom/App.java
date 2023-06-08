38aa8ddb81f9fc498a265097a3bbc5fd0972b095
bngferoz <bngferoz@gmail.com>
2023-06-08 08:38:21 +0600

    button added

--- src/main/java/vipcom/App.java
+++ src/main/java/vipcom/App.java
@@ -3,16 +3,21 @@ package vipcom;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 
 public class App extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			VBox root = new VBox();
+			Button saveBtn = new Button("Save Data");
+			root.getChildren().addAll(saveBtn);
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Roll 06");
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
